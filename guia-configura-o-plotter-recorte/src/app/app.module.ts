b1bee409472a49b8089f0a779208ab7515b2e403
Guilherme Groff <guilherme.pgb@hotmail.com>
2023-10-02 20:41:29 -0300

    Criado dois componentes - barra lateral + menu

--- guia-configura-o-plotter-recorte/src/app/app.module.ts
+++ guia-configura-o-plotter-recorte/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BarraNavegacaoLateralComponent } from './barra-navegacao-lateral/barra-navegacao-lateral.component';
+import { ItemMenuComponent } from './barra-navegacao-lateral/item-menu/item-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraNavegacaoLateralComponent,
+    ItemMenuComponent
   ],
   imports: [
     BrowserModule
