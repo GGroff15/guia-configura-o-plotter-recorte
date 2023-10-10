563d8a4c408b93cf71221e636c34a2b9d825c448
Guilherme Groff <guilherme.pgb@hotmail.com>
2023-10-10 09:22:22 -0300

    renomeado servico de processos para processo

--- guia-configura-o-plotter-recorte/src/app/services/processo.service.spec.ts
+++ guia-configura-o-plotter-recorte/src/app/services/processo.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ProcessosService } from './processos.service';
+import { ProcessosService } from './processo.service';
 
 describe('ProcessosService', () => {
   let service: ProcessosService;
