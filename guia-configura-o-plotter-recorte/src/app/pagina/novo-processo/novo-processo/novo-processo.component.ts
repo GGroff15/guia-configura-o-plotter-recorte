82bf1b2abb1121f52ad6987ed44ed15bd6085639
Guilherme Groff <guilherme.pgb@hotmail.com>
2023-10-10 13:46:53 -0300

    montado formulario cadastro processo

--- guia-configura-o-plotter-recorte/src/app/pagina/novo-processo/novo-processo/novo-processo.component.ts
+++ guia-configura-o-plotter-recorte/src/app/pagina/novo-processo/novo-processo/novo-processo.component.ts
@@ -1,7 +1,13 @@
+import { MaterialService } from './../../../services/material.service';
 import { Component, OnInit } from '@angular/core';
 import * as M from 'materialize-css';
 import { CanetaDto } from 'src/app/model/caneta-dto';
+import { LaminaDto } from 'src/app/model/lamina-dto';
+import { MaterialDto } from 'src/app/model/material-dto';
+import { TapeteDto } from 'src/app/model/tapete-dto';
 import { CanetaService } from 'src/app/services/caneta.service';
+import { LaminaService } from 'src/app/services/lamina.service';
+import { TapetesService } from 'src/app/services/tapete.service';
 
 @Component({
   selector: 'app-novo-processo',
@@ -13,14 +19,32 @@ export class NovoProcessoComponent implements OnInit {
   canetaSelecionada: string = 'Selecione uma caneta';
   canetas: CanetaDto[];
 
-  constructor(private canetaService: CanetaService) {
+  materialSelecionado: string = 'Selecione um material';
+  materiais: MaterialDto[];
+
+  tapeteSelecionado: string = 'Selecione um tapete';
+  tapetes: TapeteDto[];
+
+  laminaSelecionado: string = 'Selecione uma lâmina';
+  laminas: LaminaDto[];
+
+  tipoProcesso: string;
+
+  constructor(private canetaService: CanetaService, private materialService: MaterialService, private tapeteService: TapetesService, private laminaService: LaminaService) {
     this.canetas = canetaService.listar();
-    console.log(this.canetas);
+    this.materiais = materialService.listar();
+    this.tapetes  = tapeteService.listar();
+    this.laminas = laminaService.listar();
+    this.tipoProcesso = "Corte";
   }
 
   ngOnInit(): void {
     const select = document.querySelectorAll('select');
     M.FormSelect.init(select);
+  }
+
+  onChangeTipoProcesso(opcao: string) {
+    this.tipoProcesso = opcao;
   }
 
 }
