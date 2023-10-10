f99fb9315005fe9ee374e8fbbc3ff17566fe951b
Guilherme Groff <guilherme.pgb@hotmail.com>
2023-10-10 11:24:17 -0300

    Criado layout base tela novo processo

--- guia-configura-o-plotter-recorte/src/app/pagina/novo-processo/novo-processo/novo-processo.component.ts
+++ guia-configura-o-plotter-recorte/src/app/pagina/novo-processo/novo-processo/novo-processo.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as M from 'materialize-css';
 
 @Component({
   selector: 'app-novo-processo',
   templateUrl: './novo-processo.component.html',
   styleUrls: ['./novo-processo.component.css']
 })
-export class NovoProcessoComponent {
+export class NovoProcessoComponent implements OnInit {
+  
+  ngOnInit(): void {
+    const select = document.querySelectorAll('select');
+    M.FormSelect.init(select);
+  }
+
+  
 
 }
