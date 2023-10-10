497d4714b55f1196b29b5152a0ef6404e4a3a3f4
Guilherme Groff <guilherme.pgb@hotmail.com>
2023-10-10 10:08:05 -0300

    Criado lista processos

--- guia-configura-o-plotter-recorte/src/app/pagina/lista-processos/lista-processos.component.ts
+++ guia-configura-o-plotter-recorte/src/app/pagina/lista-processos/lista-processos.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ProcessoDto } from 'src/app/model/processo-dto';
+import { ProcessoService } from 'src/app/services/processo.service';
+import * as M from 'materialize-css';
 
 @Component({
   selector: 'app-lista-processos',
   templateUrl: './lista-processos.component.html',
   styleUrls: ['./lista-processos.component.css']
 })
-export class ListaProcessosComponent {
+export class ListaProcessosComponent implements OnInit {
+
+  processos: ProcessoDto[];
+
+  constructor(private service : ProcessoService) {
+    this.processos = service.listar('Todos');
+    console.log(this.processos);
+  }
+  ngOnInit(): void {
+    const collapsible = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsible);
+  }
 
 }
