95e2dd99da5f5519e06255a08b19fe1cc3c30703
Guilherme Groff <guilherme.pgb@hotmail.com>
2023-10-11 10:02:28 -0300

    implementado formulario reativo

--- guia-configura-o-plotter-recorte/src/app/pagina/novo-processo/novo-processo/novo-processo.component.ts
+++ guia-configura-o-plotter-recorte/src/app/pagina/novo-processo/novo-processo/novo-processo.component.ts
@@ -1,5 +1,6 @@
 import { MaterialService } from './../../../services/material.service';
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup } from '@angular/forms';
 import * as M from 'materialize-css';
 import { CanetaDto } from 'src/app/model/caneta-dto';
 import { LaminaDto } from 'src/app/model/lamina-dto';
@@ -12,30 +13,39 @@ import { TapetesService } from 'src/app/services/tapete.service';
 @Component({
   selector: 'app-novo-processo',
   templateUrl: './novo-processo.component.html',
-  styleUrls: ['./novo-processo.component.css']
+  styleUrls: ['./novo-processo.component.css'],
 })
 export class NovoProcessoComponent implements OnInit {
-  
-  canetaSelecionada: string = 'Selecione uma caneta';
   canetas: CanetaDto[];
-
-  materialSelecionado: string = 'Selecione um material';
+  
   materiais: MaterialDto[];
-
-  tapeteSelecionado: string = 'Selecione um tapete';
+  
   tapetes: TapeteDto[];
-
-  laminaSelecionado: string = 'Selecione uma lâmina';
+  
   laminas: LaminaDto[];
-
-  tipoProcesso: string;
-
-  constructor(private canetaService: CanetaService, private materialService: MaterialService, private tapeteService: TapetesService, private laminaService: LaminaService) {
-    this.canetas = canetaService.listar();
-    this.materiais = materialService.listar();
-    this.tapetes  = tapeteService.listar();
-    this.laminas = laminaService.listar();
-    this.tipoProcesso = "Corte";
+  
+  formData: FormGroup;
+  
+  constructor(
+    private canetaService: CanetaService,
+    private materialService: MaterialService,
+    private tapeteService: TapetesService,
+    private laminaService: LaminaService,
+    private formBuilder: FormBuilder
+    ) {
+      this.canetas = canetaService.listar();
+      this.materiais = materialService.listar();
+      this.tapetes = tapeteService.listar();
+      this.laminas = laminaService.listar();
+      this.formData = this.formBuilder.group({
+        canetaSelecionada: 'Selecione uma caneta',
+        materialSelecionado: 'Selecione um material',
+        tapeteSelecionado: 'Selecione um tapete',
+        laminaSelecionado: 'Selecione uma lâmina',
+        profundidadeLamina: '',
+        pressaoFerramenta: '',
+        tipoProcesso: 'Corte'
+    });
   }
 
   ngOnInit(): void {
@@ -43,8 +53,7 @@ export class NovoProcessoComponent implements OnInit {
     M.FormSelect.init(select);
   }
 
-  onChangeTipoProcesso(opcao: string) {
-    this.tipoProcesso = opcao;
+  getFormData(): AbstractControl<any, any> {
+    return this.formData.value;
   }
-
 }
